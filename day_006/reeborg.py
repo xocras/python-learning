df50f6a75e51d431e9c2eafe7f20bf1cd7d09443
Oscar Cruz <oscarfcr.works@gmail.com>
2024-01-20 18:33:49 -0400

    Uploaded Day 6

---
+++ day_006/reeborg.py
@@ -0,0 +1,54 @@
+# sound(True)
+
+# Commands
+
+# def turn_right():
+#     for step in range(0, 3):
+#         turn_left()
+#
+#
+# def forward():
+#     if front_is_clear(): move()
+#     while front_is_clear() and not right_is_clear():
+#         move()
+
+# Hurdles
+
+# def jump():
+#     turn_left()
+#
+#     while wall_on_right():
+#         move()
+#
+#     turn_right()
+#     move()
+#     turn_right()
+#
+#     while front_is_clear():
+#         move()
+#
+#     turn_left()
+#
+# while not at_goal():
+#     if front_is_clear():
+#         move()
+#     if wall_in_front():
+#         jump()
+
+# Maze
+
+# while front_is_clear():
+#     move()
+#
+# while right_is_clear():
+#     turn_left()
+#
+# while not at_goal():
+#
+#     if front_is_clear():
+#         move()
+#     else:
+#         turn_left()
+#
+#     if right_is_clear() and not at_goal():
+#         turn_right()
