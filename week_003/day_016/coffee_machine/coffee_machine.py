680567271369e2498849e077c46d8ae90815cc4c
Oscar Cruz <oscarfcr.works@gmail.com>
2024-01-24 06:32:36 -0400

    Refactored: Coffee Machine
    Signed-off-by: Oscar Cruz <oscarfcr.works@gmail.com>

--- week_003/day_016/coffee_machine/coffee_machine.py
+++ week_003/day_016/coffee_machine/coffee_machine.py
@@ -1,6 +1,6 @@
-from rsc.menu import Menu, MenuItem
-from rsc.coffee_maker import CoffeeMaker
-from rsc.money_machine import MoneyMachine
+from menu import Menu
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
 
 menu = Menu()
 
