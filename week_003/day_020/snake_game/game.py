0fddebbb3e7756bc46c895ffa1429260a44e8c97
Oscar Cruz <oscarfcr.works@gmail.com>
2024-01-27 15:10:00 -0400

    Added Recursion.
    Signed-off-by: Oscar Cruz <oscarfcr.works@gmail.com>

--- week_003/day_020/snake_game/game.py
+++ week_003/day_020/snake_game/game.py
@@ -1,22 +1,35 @@
 from turtle import Screen
 from snake import Snake
-
+from time import sleep
 
 SCREEN_SIZE = 600
 
 screen = Screen()
-screen.tracer(0)
-screen.bgcolor("black")
-screen.title("Snake Game")
-screen.setup(SCREEN_SIZE, SCREEN_SIZE)
 
 snake = Snake(screen)
 
-screen.listen()
 
-screen.onkeypress(snake.turn_north, "Up")
-screen.onkeypress(snake.turn_south, "Down")
-screen.onkeypress(snake.turn_east, "Right")
-screen.onkeypress(snake.turn_west, "Left")
+def run_game():
+    sleep(0.1)
+    snake.move()
+    run_game()
+
+
+def setup_screen():
+    screen.tracer(0)
+    screen.bgcolor("black")
+    screen.title("Snake Game")
+    screen.setup(SCREEN_SIZE, SCREEN_SIZE)
+
+    screen.listen()
+
+    screen.onkey(snake.turn_north, "Up")
+    screen.onkey(snake.turn_south, "Down")
+    screen.onkey(snake.turn_east, "Right")
+    screen.onkey(snake.turn_west, "Left")
+
+
+setup_screen()
+run_game()
 
 screen.exitonclick()
