3803c72f3deb40599a31bed5822161aefc4d1922
Oscar Cruz <oscarfcr.works@gmail.com>
2024-01-27 13:39:45 -0400

    Refactored functions.
    Signed-off-by: Oscar Cruz <oscarfcr.works@gmail.com>

--- week_003/day_020/snake.py
+++ week_003/day_020/snake.py
@@ -12,21 +12,21 @@ screen.title("Snake Game")
 screen.setup(SCREEN_DIMENSION, SCREEN_DIMENSION)
 
 
-def section_position(snake):
-    return snake[len(snake) - 1].xcor() - SNAKE_DIMENSION if len(snake) > 0 else 0
+def section_position():
+    return PLAYER[len(PLAYER) - 1].xcor() - SNAKE_DIMENSION if len(PLAYER) > 0 else 0
 
 
-def add_segments(snake, amount):
+def add_segments(amount):
     for n in range(amount):
         snake_segments = Turtle("square")
         snake_segments.color("white")
         snake_segments.penup()
-        snake_segments.setx(section_position(snake))
-        snake.append(snake_segments)
+        snake_segments.setx(section_position())
+        PLAYER.append(snake_segments)
     screen.update()
 
 
-def move_tail(x_offset, y_offset):
+def move_tail():
     for segment in range(len(PLAYER)-1, 0, -1):
 
         x = PLAYER[segment - 1].xcor()
@@ -39,7 +39,7 @@ def turn_east():
     if PLAYER[0].heading() == 180:
         return
 
-    move_tail(-SNAKE_DIMENSION/2, 0)
+    move_tail()
 
     PLAYER[0].setheading(0)
     PLAYER[0].forward(SNAKE_DIMENSION)
@@ -51,7 +51,7 @@ def turn_west():
     if PLAYER[0].heading() == 0:
         return
 
-    move_tail(SNAKE_DIMENSION/2, 0)
+    move_tail()
 
     PLAYER[0].setheading(180)
     PLAYER[0].forward(SNAKE_DIMENSION)
@@ -63,7 +63,7 @@ def turn_south():
     if PLAYER[0].heading() == 90:
         return
 
-    move_tail(0, SNAKE_DIMENSION/2)
+    move_tail()
 
     PLAYER[0].setheading(270)
     PLAYER[0].forward(SNAKE_DIMENSION)
@@ -75,7 +75,7 @@ def turn_north():
     if PLAYER[0].heading() == 270:
         return
 
-    move_tail(0, -SNAKE_DIMENSION/2)
+    move_tail()
 
     PLAYER[0].setheading(90)
     PLAYER[0].forward(SNAKE_DIMENSION)
@@ -83,7 +83,7 @@ def turn_north():
     screen.update()
 
 
-add_segments(PLAYER, STARTING_LENGTH)
+add_segments(STARTING_LENGTH)
 
 screen.listen()
 
