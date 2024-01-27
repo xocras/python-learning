a63808bf90baa0fe6c75ab31194684eb3889ce46
Oscar Cruz <oscarfcr.works@gmail.com>
2024-01-26 22:45:40 -0400

    Added Movement
    Signed-off-by: Oscar Cruz <oscarfcr.works@gmail.com>

--- week_003/day_021/snake.py
+++ week_003/day_021/snake.py
@@ -2,6 +2,7 @@ from turtle import Turtle, Screen
 
 SCREEN_DIMENSION = 600
 SNAKE_DIMENSION = 20
+SNAKE_SPEED = 10
 STARTING_LENGTH = 3
 PLAYER = []
 
@@ -12,17 +13,26 @@ screen.setup(SCREEN_DIMENSION, SCREEN_DIMENSION)
 
 
 def section_position(snake):
-    return snake[len(snake) - 1].xcor() + SNAKE_DIMENSION if len(snake) > 0 else 0
+    return snake[len(snake) - 1].xcor() - SNAKE_DIMENSION if len(snake) > 0 else 0
 
 
-def add_sections(snake, amount):
+def add_segments(snake, amount):
     for n in range(amount):
-        snake_section = Turtle("square")
-        snake_section.color("white")
-        snake_section.setx(section_position(snake))
-        snake.append(snake_section)
+        snake_segments = Turtle("square")
+        snake_segments.color("white")
+        snake_segments.penup()
+        snake_segments.setx(section_position(snake))
+        snake.append(snake_segments)
 
 
-add_sections(PLAYER, STARTING_LENGTH)
+def move_snake():
+    for snake_section in PLAYER:
+        snake_section.forward(SNAKE_SPEED)
+
+
+add_segments(PLAYER, STARTING_LENGTH)
+
+screen.listen()
+screen.onkeypress(move_snake, "Right")
 
 screen.exitonclick()
