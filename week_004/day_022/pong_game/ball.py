b0d01afe9205a4ba87da42e90d5080e55984bb33
Oscar Cruz <oscarfcr.works@gmail.com>
2024-01-28 11:38:27 -0400

    Speed Increases on Bounce.
    Signed-off-by: Oscar Cruz <oscarfcr.works@gmail.com>

--- week_004/day_022/pong_game/ball.py
+++ week_004/day_022/pong_game/ball.py
@@ -1,11 +1,14 @@
 from turtle import Turtle
 
-SPEED = 10
-
 
 class Ball(Turtle):
     def __init__(self):
         super().__init__("circle")
+        self.last_hit = ""
+        self.bounces = 0
+        self.size = 20
+        self.speed = 10
+
         self.margin = 20
         self.x_bound = 400
         self.y_bound = 300
@@ -38,15 +41,37 @@ class Ball(Turtle):
     def x_collision(self, x):
         return not -self.x_bound + self.margin <= x <= self.x_bound - self.margin
 
+    def paddle_collision(self, paddle):
+        check = self.distance(paddle) <= self.size + self.speed and self.last_hit != paddle.name
+        if check:
+            self.last_hit = paddle.name
+        return check
+
+    def paddle_hit(self):
+        print(f"{self.last_hit} hit the ball!")
+
+    def set_speed(self, amount):
+        self.speed = amount
+
+    def increase_speed(self, amount):
+        self.speed += amount
+
+    def increase_bounces(self, amount):
+        self.bounces += amount
+        if not self.bounces % 5:
+            self.increase_speed(1)
+
     def move(self):
-        x = self.xcor() + SPEED * self.x_direction
-        y = self.ycor() + SPEED * self.y_direction
+        x = self.xcor() + self.speed * self.x_direction
+        y = self.ycor() + self.speed * self.y_direction
 
         if self.y_collision(y):
             self.switch_y_direction()
+            self.increase_bounces(1)
 
         if self.x_collision(x):
             self.switch_x_direction()
+            self.increase_bounces(1)
 
         self.setposition(x, y)
 
