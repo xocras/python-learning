0caa57f7d596b41a5b69dd71a493cd12f291028e
Oscar Cruz <oscarfcr.works@gmail.com>
2024-01-28 19:43:21 -0400

    Mail Merge Challenge.
    Signed-off-by: Oscar Cruz <oscarfcr.works@gmail.com>

---
+++ week_004/day_024/mail_merge/main.py
@@ -0,0 +1,21 @@
+# Create a letter using starting_letter.txt
+# for each name in invited_names.txt
+# Replace the [name] placeholder with the actual name.
+# Save the letters in the folder "ready".
+
+# Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
+# Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
+# Hint3: This method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+PLACEHOLDER = "[name]"
+
+with open("./input/letters/starting_letter.txt") as file:
+    starting_letter = file.read()
+
+with open("./input/names/invited_names.txt") as file:
+    invited_names = [name.strip() for name in file.readlines()]
+
+for name in invited_names:
+    output = starting_letter.replace(PLACEHOLDER, name)
+    with open(f"./output/ready/letter_for_{name.lower()}", mode="w") as file:
+        file.write(output)
