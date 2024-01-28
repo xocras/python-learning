b48256a5c24c54fb2196e6a4b723dfe00113e0c7
Oscar Cruz <oscarfcr.works@gmail.com>
2024-01-28 19:45:21 -0400

    Added Output File Extension.
    Signed-off-by: Oscar Cruz <oscarfcr.works@gmail.com>

--- week_004/day_024/mail_merge/output/ready/main.py
+++ week_004/day_024/mail_merge/output/ready/main.py
@@ -9,13 +9,13 @@
 
 PLACEHOLDER = "[name]"
 
-with open("./input/letters/starting_letter.txt") as file:
+with open("../../input/letters/starting_letter.txt") as file:
     starting_letter = file.read()
 
-with open("./input/names/invited_names.txt") as file:
+with open("../../input/names/invited_names.txt") as file:
     invited_names = [name.strip() for name in file.readlines()]
 
 for name in invited_names:
     output = starting_letter.replace(PLACEHOLDER, name)
-    with open(f"./output/ready/letter_for_{name.lower()}", mode="w") as file:
+    with open(f"./output/ready/letter_for_{name.lower()}.txt", mode="w") as file:
         file.write(output)
