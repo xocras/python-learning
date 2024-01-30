c186a93ff535373f6db4e4f8ef5f1c476460591e
Oscar Cruz <oscarfcr.works@gmail.com>
2024-01-29 23:18:37 -0400

    Study Files are created on Game Over.
    Signed-off-by: Oscar Cruz <oscarfcr.works@gmail.com>

--- week_004/day_025/states_game/main.py
+++ week_004/day_025/states_game/main.py
@@ -6,9 +6,13 @@ import pandas
 # Notes:
 # Series.item(): Returns the first item
 # Series.to_list(): Converts series into a list
+# ~ Can be used as a not operator
 
 
 def end_game():
+    # Create States to Learn CSV
+    complement = states['state'][~states['state'].isin(pandas.Series(answers.keys()))]
+    complement.to_csv("states_to_learn.csv", index=False)
 
     # Clear Answers
     for answer in answers.values():
@@ -34,7 +38,13 @@ answers = {}
 
 while len(answers) < 50:
 
-    guess = turtle.textinput(f"Guess the State - {len(answers)}/50", "Name another state:").title()
+    guess = turtle.textinput(f"Guess the State - {len(answers)}/50", "Name another state:")
+
+    if not len(guess):
+        end_game()
+        break
+
+    guess = guess.title()
 
     if guess in answers.keys():
         continue
