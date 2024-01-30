207a8c13a7278f4b4225daf316fa31b2b05c2c84
Oscar Cruz <oscarfcr.works@gmail.com>
2024-01-29 21:52:00 -0400

    Squirrel Challenge.
    Signed-off-by: Oscar Cruz <oscarfcr.works@gmail.com>

---
+++ week_004/day_025/squirrel_challenge.py
@@ -0,0 +1,16 @@
+import pandas
+
+data = pandas.read_csv("squirrel_data.csv")
+
+fur_colors = data['Primary Fur Color'].dropna().unique()
+
+table = {
+    'Primary Fur Color': [color for color in fur_colors],
+    'Count': [data['Primary Fur Color'].value_counts()[color] for color in fur_colors]
+}
+
+table = pandas.DataFrame(table)
+
+table.to_csv("squirrel_count.csv", index=False)
+
+print(table)
