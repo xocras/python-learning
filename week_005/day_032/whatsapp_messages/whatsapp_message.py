b6b3b150605a5a1f1d9ef00b3a417febd192e1ae
Oscar Cruz <oscarfcr.works@gmail.com>
2024-02-03 19:43:46 -0400

    Whatsapp Messaging.
    Signed-off-by: Oscar Cruz <oscarfcr.works@gmail.com>

---
+++ week_005/day_032/whatsapp_messages/whatsapp_message.py
@@ -0,0 +1,9 @@
+import pywhatkit as whatsapp
+
+phone_numbers = ["+123456789"]
+image_path = "image.png"
+message = "Hello from Python! This is an instant WhatsApp message."
+
+for phone_number in phone_numbers:
+    whatsapp.sendwhats_image(phone_number, image_path, message, 15, True, 5)
+
