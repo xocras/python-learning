a2583bc9a284483b8174daffc4c8b8ad2f943d13
Oscar Cruz <oscarfcr.works@gmail.com>
2024-02-08 20:52:39 -0400

    Added custom requests module.

---
+++ week_006/day_037/custom_requests.py
@@ -0,0 +1,45 @@
+import requests
+
+import custom_logging as logging
+
+
+def get(endpoint: str, **parameters) -> dict:
+
+    response = requests.get(endpoint, parameters)
+
+    logging.info(response.text)
+
+    response.raise_for_status()
+
+    return response.json()
+
+
+def post(endpoint: str, parameters: dict, headers: dict = None):
+
+    response = requests.post(endpoint, json=parameters, headers=headers)
+
+    logging.info(response.text)
+
+    response.raise_for_status()
+
+
+def put(endpoint: str, parameters: dict, headers: dict = None):
+    response = requests.put(endpoint, json=parameters, headers=headers)
+
+    logging.info(response.text)
+
+    try:
+        response.raise_for_status()
+    except requests.exceptions.HTTPError:
+        logging.error("Update Failed. Try Again.")
+
+
+def delete(endpoint: str, headers: dict = None):
+    response = requests.delete(endpoint, headers=headers)
+
+    logging.info(response.text)
+
+    try:
+        response.raise_for_status()
+    except requests.exceptions.HTTPError:
+        logging.error("Update Failed. Try Again.")
