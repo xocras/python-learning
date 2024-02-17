03e7e8af25189f2cec49199c89cc3970ffdf4106
Oscar Cruz <oscarfcr.works@gmail.com>
2024-02-17 08:03:59 -0400

    Scraping Bots

---
+++ week_008/day_051/twitter_bot.py
@@ -0,0 +1,37 @@
+from selenium import webdriver
+
+from selenium.webdriver.common.by import By
+
+from selenium.webdriver.support.ui import WebDriverWait
+
+from selenium.webdriver.support import expected_conditions as EC
+
+
+def main():
+    # Browser parameters
+
+    website = 'https://twitter.com/home'
+
+    user = '-'
+
+    password = '-'
+
+    #  Keep browser instance open
+
+    settings = webdriver.ChromeOptions()
+
+    settings.add_experimental_option('detach', True)
+
+    # Open website
+
+    browser = webdriver.Chrome(options=settings)
+
+    browser.get(f'{website}')
+
+    # Set timeout
+
+    wait = WebDriverWait(browser, 3600)
+
+
+if __name__ == '__main__':
+    main()
