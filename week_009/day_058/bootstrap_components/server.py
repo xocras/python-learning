242b08fcc2b275ad1482613e4abdff1b2899f80a
Oscar Cruz <oscarfcr.works@gmail.com>
2024-02-19 21:45:33 -0400

    Uploaded Day 58

---
+++ week_009/day_058/bootstrap_components/server.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template(
+        'index.html',
+        year=datetime.now().year
+    )
+
+
+def main():
+    app.run(debug=True)
+
+
+if __name__ == '__main__':
+    main()
