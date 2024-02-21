4dfe65493af4c881bb7cacb298f1aed7f91a8fe1
Oscar Cruz <oscarfcr.works@gmail.com>
2024-02-20 20:54:34 -0400

    Uploaded Day 58

--- week_009/day_058/tindog/server.py
+++ week_009/day_058/tindog/server.py
@@ -7,10 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template(
-        'index.html',
-        year=datetime.now().year
-    )
+    return render_template('index.html')
 
 
 def main():
