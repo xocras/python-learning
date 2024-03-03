6f620e7d4f77b944158ad21313b45cf990e32cb8
Oscar Cruz <oscarfcr.works@gmail.com>
2024-03-03 07:06:23 -0400

    Updated Day 63

--- week_009/day_063/book_library/server.py
+++ week_009/day_063/book_library/server.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, redirect
 from flask_bootstrap import Bootstrap5
 from flask_wtf import FlaskForm
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Column, String, Integer, Float, CheckConstraint
+from sqlalchemy import String, Integer, Float, CheckConstraint
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from wtforms import StringField, DecimalField, SubmitField
 from wtforms.validators import DataRequired, Length, NumberRange
+
+
+class Base(DeclarativeBase):
+    pass
 
 
 app = Flask(__name__)
@@ -15,14 +20,16 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///books-collection.db"
 
 Bootstrap5(app)
 
-db = SQLAlchemy(app)
+db = SQLAlchemy(model_class=Base)
+
+db.init_app(app)
 
 
 class Book(db.Model):
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), unique=True, nullable=False)
-    author = Column(String(250), nullable=False)
-    rating = Column(Float, nullable=False)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    author: Mapped[str] = mapped_column(String(250), nullable=False)
+    rating: Mapped[float] = mapped_column(Float, nullable=False)
 
     __table_args__ = (
         CheckConstraint('rating >= 0 AND rating <= 10', name='check_rating'),
@@ -39,15 +46,14 @@ class BookForm(FlaskForm):
     submit = SubmitField('Add Book')
 
 
+class BookEditForm(FlaskForm):
+    rating = DecimalField('Rating', validators=[DataRequired(), NumberRange(0, 10)])
+    submit = SubmitField('Edit Book')
+
+
 @app.route('/')
 def index():
-    books = [
-        {
-            'name': book.name,
-            'author': book.author,
-            'rating': f'{book.rating:.1f}' if book.rating % 1 else f'{book.rating:.0f}'
-        } for book in Book.query.all()
-    ]
+    books = db.session.execute(db.select(Book).order_by(Book.id)).scalars().all()
 
     return render_template('index.html', books=books)
 
@@ -58,22 +64,51 @@ def add():
 
     if form.validate_on_submit():
 
-        db.session.add(
-            Book(
+        book = Book(
                 name=form.name.data,
                 author=form.author.data,
                 rating=form.rating.data
-            )
         )
+
+        db.session.add(book)
 
         db.session.commit()
 
-        return index()
+        return redirect(url_for("edit", book_id=book.id))
 
     return render_template('add.html', form=form)
 
 
+@app.route('/book/<int:book_id>/edit', methods=['GET', 'POST'])
+def edit(book_id):
+    form = BookEditForm()
+
+    book = db.get_or_404(Book, book_id)
+
+    if form.validate_on_submit():
+        book.rating = form.rating.data
+
+        db.session.commit()
+
+        return redirect(url_for('index'))
+
+    return render_template("edit.html", book=book, form=form)
+
+
+@app.route("/book/<int:book_id>/delete", methods=["GET", "POST"])
+def delete(book_id):
+
+    book = db.get_or_404(Book, book_id)
+
+    db.session.delete(book)
+
+    db.session.commit()
+
+    return redirect(url_for('index'))
+
+
 def main():
+
     with app.app_context():
         db.create_all()
 
