e494130ac769295ead9a1dfc56c363032eed4712
Oscar Cruz <oscarfcr.works@gmail.com>
2024-03-03 20:51:17 -0400

    Updated Day 66

--- week_010/day_066/server.py
+++ week_010/day_066/server.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String, Boolean
+from random import choice
 
 app = Flask(__name__)
 
@@ -31,17 +32,21 @@ class Cafe(db.Model):
     can_take_calls: Mapped[bool] = mapped_column(Boolean, nullable=False)
     coffee_price: Mapped[str] = mapped_column(String(250), nullable=True)
 
+    def to_dict(self):
+        return {column.name: getattr(self, column.name) for column in self.__table__.columns}
 
-with app.app_context():
-    db.create_all()
 
-
-@app.route("/")
+@app.route('/')
 def home():
     return render_template("index.html")
 
 
 # HTTP GET - Read Record
+@app.route('/random', methods=['GET'])
+def random():
+    cafes = db.session.execute(db.select(Cafe)).scalars().all()
+
+    return jsonify(cafe=choice(cafes).to_dict())
 
 # HTTP POST - Create Record
 
@@ -51,4 +56,7 @@ def home():
 
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+
     app.run(debug=True)
