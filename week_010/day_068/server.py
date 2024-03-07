36658b0581b6246f1ee7dc64bab4a1895f065710
Oscar Cruz <oscarfcr.works@gmail.com>
2024-03-07 08:38:38 -0400

    Updated Day 68

--- week_010/day_068/server.py
+++ week_010/day_068/server.py
@@ -1,3 +1,4 @@
+from os import getenv
 from flask import Flask, render_template, request, url_for, redirect, flash, send_from_directory
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
@@ -6,7 +7,7 @@ from sqlalchemy import Integer, String
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret-key-goes-here'
+app.config['SECRET_KEY'] = getenv('SECRET_KEY')
 
 
 # CREATE DATABASE
