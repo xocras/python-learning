a98555823686263f1ec325ac7e65a15e1d7388e3
Oscar Cruz <oscarfcr.works@gmail.com>
2024-03-10 07:19:55 -0400

    Updated Day 68

--- week_010/day_068/server.py
+++ week_010/day_068/server.py
@@ -1,4 +1,4 @@
-from os import getenv
+from os import getenv, getcwd
 from flask import Flask, render_template, request, url_for, redirect, flash, send_from_directory
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
@@ -6,21 +6,25 @@ from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = getenv('SECRET_KEY')
-
 
 # CREATE DATABASE
 class Base(DeclarativeBase):
     pass
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = getenv('SECRET_KEY')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+
 db = SQLAlchemy(model_class=Base)
+
 db.init_app(app)
 
 
-# CREATE TABLE IN DB
+# CONFIGURE TABLE
+
 class User(db.Model):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     email: Mapped[str] = mapped_column(String(100), unique=True)
@@ -37,8 +41,28 @@ def home():
     return render_template("index.html")
 
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
+    if request.method == 'POST':
+
+        email = request.form.get('email')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            return render_template("register.html", message="This e-mail is already in use.")
+
+        user = User(
+            name=request.form.get('name'),
+            password=request.form.get('password'),
+            email=email
+        )
+
+        db.session.add(user)
+
+        db.session.commit()
+
+        return render_template("secrets.html", user=user)
+
     return render_template("register.html")
 
 
